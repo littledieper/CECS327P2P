ccfbc58069028a13a82c4415f9e202ec556df264
Alex <alexdiep96@gmail.com>
2017-11-18 22:20:22 -0800

    Update Client/Server to work on dispatcher model. Add ServerTask. Add full push/pull support.

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,76 +1,97 @@
 package com.company;
+import com.company.file.FileHandler;
+import com.company.file.SerialFileAttr;
+
 import java.net.*;
 import java.io.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
+import java.util.HashSet;
 
 public class Client extends NetworkProtocol implements Runnable
 {
     private String serverName;
-    private Socket client;
     private int port;
-    private String threadMessage;
 
     public static void main(String [] args)
     {
+        Client client = new Client("127.0.0.1");
+        new Thread(client).start();
     }
 
     public Client()
     {
-        this.serverName = "0.0.0.0";
-        this.port = 21;
+        this("0.0.0.0", 21, "external1" + File.separatorChar);
     }
 
     public Client(String serverName)
     {
-        this.serverName = serverName;
-        this.port = 21;
+        this(serverName, 21, "external1" + File.separatorChar);
     }
 
     public Client(String serverName, int port)
     {
-        this.serverName = serverName;
-        this.port = port;
+        this(serverName, port, "external1" + File.separatorChar);
     }
 
-    public Client(String serverName, int port, String message)
+    public Client(String serverName, int port, String directory)
     {
         this.serverName = serverName;
         this.port = port;
-        this.threadMessage = message;
+        this.directory = directory;
     }
 
+    /**
+     * Main function that gets called when the thread that has this Runnable class starts. This will synchronize the
+     * files in their respective directories both ways.
+     *
+     * Operation is as follows:
+     * 1) Get this computer's local files.
+     * 2) Get the remote computer's local files.
+     * 3) Compare the list of files and get the list of files we want to get from the remote computer.
+     * 4) Send the list of files we want to pull to the remote computer, then pull the files.
+     * 5) Compare the list of files again, but get the list of files we want to push.
+     * 6) Send the list of files we want to push to the remote computer, then push the files.
+     * 7) Local and remote files computers (only in the DIR's) are sync'ed.
+     */
+    @Override
     public void run()
     {
+        // We're going to be instantiating client objects on demand per node.
         connectToServer();
-        if(this.client == null){
+        if(this.socket == null){
             return;
         }
 
+        // Get this computer's local files and the remote computer's files and compare them to determine the files we want to pull.
+        HashSet<SerialFileAttr> localFiles = FileHandler.getAllLocalFileInfo(directory);
+        HashSet<SerialFileAttr> remoteFiles = receiveFileInfo();
+        HashSet<SerialFileAttr> filesToPull = compare(localFiles, remoteFiles);
 
-        FileTransfer clientFT;
+        // Let the remote PC know which files we want (as comparison is a local process) and pull the files into the local directory.
+        sendFileInfo(filesToPull);
+        for (SerialFileAttr fileToPull: filesToPull) {
+            receiveFile(fileToPull);
+        }
 
-        try{
-            ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-            FileTransfer ft = InputFileAttr(in);
-            if(ft != null)
-                clientFT = new FileTransfer();
-            System.out.println("Null object");
-            in.close();
-        }catch(Exception e){System.out.println(e.toString());}
+        // Now we work on pushing the local files to remote computer, so compare to get list of files to push.
+        // We don't want (nor do we care about) the local changes made to the local directory after we pulled updated files.
+        HashSet<SerialFileAttr> filesToPush = compare(remoteFiles, localFiles);
+        sendFileInfo(filesToPush);
+        for (SerialFileAttr fileToPush: filesToPush) {
+            sendFile(fileToPush);
+        }
 
-        closeClient();
+        // Cleanup
+        closeSocket();
     }
 
+    /**
+     * Creates a Socket + makes a connection to the remote computer.
+     */
     private void connectToServer(){
-        this.client = null;
         try {
-            System.out.println("Connecting to " + serverName
-                    + " on port " + this.port);
-            this.client = new Socket(serverName, this.port);
-            System.out.println("Just connected to "
-                    + client.getRemoteSocketAddress());
+            System.out.println("Connecting to " + serverName  + " on port " + this.port);
+            this.socket = new Socket(serverName, this.port);
+            System.out.println("Just connected to " + socket.getRemoteSocketAddress());
         } catch(SocketTimeoutException s) {
             System.out.println("Socket timed out!");
         } catch(IOException e){
@@ -78,15 +99,4 @@ public class Client extends NetworkProtocol implements Runnable
             e.printStackTrace();
         }
     }
-
-    private void closeClient(){
-        try{
-            this.client.close();
-        } catch(IOException e){
-            System.out.println("Client failed to close or is null");
-            e.printStackTrace();
-        }
-    }
-
-
 }
